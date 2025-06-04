7e19977570ce7b2ec5b325f9ad793799a7c62cf8
Geethasree Manikonda <158323003+hehhhehehhhehhehehehe@users.noreply.github.com>
2025-06-04 13:47:25 -0500

    initial creating database.sql

--- code/initial creating database.sql
+++ code/initial creating database.sql
@@ -1,4 +1,18 @@
-Here iam using MYSQL WORKBENCH for my code:
+/*
+-----------------------------------------------
+Creating databases and schemas:
+-----------------------------------------------
+script: This creates new database "Datawarehouse"  after checking it is dropped entirely
+or not if it needs to drop use DROP DATABASE datewarehouse and create a database with three schemas 
+Bronze, silver and gold.
+------------------------------------------------
+warning : this script will drop everything and data will be deleted permanently so make sure  of everything 
+before running this code.
+------------------------------------------------
+*/
+
+
+# Here iam using MYSQL WORKBENCH for my code:
 
 # Dropping a database named "Datawarehouse" if EXISTS and creating a new database called "Datawarehouse"
 
@@ -6,13 +20,14 @@ DROP DATABASE Datawarehouse;
 CREATE DATABASE Datawarehouse;
 USE Datawarehouse;
 
-Here we are creating schema for our database "Datawarehouse"  (# as creating another three databases)
-DROP IF SCHEMAS EXISTS:
+# Here we are creating schema for our database "Datawarehouse"  (# as creating another three databases)
+
+    DROP IF SCHEMAS EXISTS:
     DROP SCHEMA bronze;
     DROP SCHEMA silver;
     DROP SCHEMA gold;
 
-CREATING NEW SCHEMAS:
+# CREATING NEW SCHEMAS:
     CREATE SCHEMA bronze;
     CREATE SCHEMA silver;
     CREATE SCHEMA gold;
