46012a33748712b98a2d0cee7002c781164316da
Geethasree Manikonda <158323003+hehhhehehhhehhehehehe@users.noreply.github.com>
2025-06-08 19:10:28 -0500

    Create select the tables.sql

---
+++ code/select the tables.sql
@@ -0,0 +1,17 @@
+/*
+-----------------------------------------------------------
+script : writing the select statements in order to get the data table.
+warnings : need to use database with the select statement or else will throw error.
+parameters: cannot be added only data table will be generated.
+------------------------------------------------------------
+*/
+select * from bronze_crm_cust_info;
+select * from bronze.crm_prd_info;
+select * from bronze.crm_sales_details;
+select * from bronze.erp_cust_az12;
+select * from bronze.erp_loc_a101;
+select * from bronze.erp_px_cat_g1v2;
+
+
+
+
